7233e2ef0131b3eacd62f5c1f2b0ba86b5066024
Simon Norman <simon.norman@spacelab.local>
2018-09-07 17:43:30 +0100

    Rough proof of concept for schedule usage analysis

--- services/recordings_retrieval/space_usage_analysis_scheduler.js
+++ services/recordings_retrieval/space_usage_analysis_scheduler.js
@@ -6,10 +6,21 @@ const schedule = require('node-schedule');
 module.exports = () => stampit({
   methods: {
     scheduleUsageAnalysis(minutesOfTheHour, functionToCall) {
+      const getRecordings = (fireDate) => {
+        const usageAnalysisPeriod = 900000;
+        const params = {
+          areaId: '1A',
+          startTime: fireDate - usageAnalysisPeriod,
+          endTime: fireDate,
+        };
+
+        functionToCall();
+      };
+
       const rule = new schedule.RecurrenceRule();
       rule.minute = minutesOfTheHour;
       schedule.scheduleJob('*/1 * * * * *', (fireDate) => {
-        functionToCall(fireDate);
+        getRecordings(fireDate);
       });
     },
   },
