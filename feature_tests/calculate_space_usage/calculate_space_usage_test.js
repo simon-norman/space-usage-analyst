6a2b566f19e17b1ec3bc881d701a8c975a005aba
unknown <01secn@gmail.com>
2018-10-09 17:48:27 +0100

    Added test to check that errors thrown by save space usage api calls are bubbled up

--- feature_tests/calculate_space_usage/calculate_space_usage_test.js
+++ feature_tests/calculate_space_usage/calculate_space_usage_test.js
@@ -64,35 +64,56 @@ describe('Space usage calculator', function () {
     setUpExpectedSpaceUsagesToBeCalculated();
   });
 
-  context('when recording and spaces data to calculate usage is available from apis', function () {
-    it('should calculate, for the specified timeframe, the space usage for each area', async function () {
-      wifiRecordingsSpaceUsageCalculator.calculateSpaceUsage(calculateSpaceUsageParams);
+  context('given recording and spaces to calculate usage are available from apis,', function () {
+    context('and that space usage api is available to save space usage,', function () {
+      context('when calculate space usage is called ONCE with a specified timeframe', function () {
+        it('then should calculate and save, for that timeframe, the space usage for each area', async function () {
+          wifiRecordingsSpaceUsageCalculator.calculateSpaceUsage(calculateSpaceUsageParams);
 
-      await setPromisifiedTimeout(1);
+          await setPromisifiedTimeout(1);
 
-      const firstSpaceUsagePostedToMockSpaceUsageApi = postSpaceUsageStub.firstCall.args[1];
-      expect(firstSpaceUsagePostedToMockSpaceUsageApi.variables.input)
-        .deep.equals(spaceId1ExpectedSpaceUsageToBeCalculated);
+          const firstSpaceUsagePostedToMockSpaceUsageApi = postSpaceUsageStub.firstCall.args[1];
+          expect(firstSpaceUsagePostedToMockSpaceUsageApi.variables.input)
+            .deep.equals(spaceId1ExpectedSpaceUsageToBeCalculated);
 
-      const secondSpaceUsagePostedToMockSpaceUsageApi = postSpaceUsageStub.secondCall.args[1];
-      expect(secondSpaceUsagePostedToMockSpaceUsageApi.variables.input)
-        .deep.equals(spaceId2ExpectedSpaceUsageToBeCalculated);
+          const secondSpaceUsagePostedToMockSpaceUsageApi = postSpaceUsageStub.secondCall.args[1];
+          expect(secondSpaceUsagePostedToMockSpaceUsageApi.variables.input)
+            .deep.equals(spaceId2ExpectedSpaceUsageToBeCalculated);
+        });
+      });
     });
 
-    it('should not duplicate space usage calculations (duplicates could be caused by space usage calculator adding duplicate listeners)', async function () {
-      wifiRecordingsSpaceUsageCalculator.calculateSpaceUsage(calculateSpaceUsageParams);
-      wifiRecordingsSpaceUsageCalculator.calculateSpaceUsage(calculateSpaceUsageParams);
+    context('when calculate space usage is called TWICE in quick succession', function () {
+      it('should not duplicate space usage calculations (duplicates could be caused by space usage calculator adding duplicate listeners)', async function () {
+        wifiRecordingsSpaceUsageCalculator.calculateSpaceUsage(calculateSpaceUsageParams);
+        wifiRecordingsSpaceUsageCalculator.calculateSpaceUsage(calculateSpaceUsageParams);
 
-      await setPromisifiedTimeout(1);
+        await setPromisifiedTimeout(1);
 
-      const firstSpaceUsagePostedToMockSpaceUsageApi = postSpaceUsageStub.firstCall.args[1];
-      expect(firstSpaceUsagePostedToMockSpaceUsageApi.variables.input)
-        .deep.equals(spaceId1ExpectedSpaceUsageToBeCalculated);
-      expect(postSpaceUsageStub).to.have.callCount(4);
+        const firstSpaceUsagePostedToMockSpaceUsageApi = postSpaceUsageStub.firstCall.args[1];
+        expect(firstSpaceUsagePostedToMockSpaceUsageApi.variables.input)
+          .deep.equals(spaceId1ExpectedSpaceUsageToBeCalculated);
+        expect(postSpaceUsageStub).to.have.callCount(4);
 
-      const secondSpaceUsagePostedToMockSpaceUsageApi = postSpaceUsageStub.secondCall.args[1];
-      expect(secondSpaceUsagePostedToMockSpaceUsageApi.variables.input)
-        .deep.equals(spaceId2ExpectedSpaceUsageToBeCalculated);
+        const secondSpaceUsagePostedToMockSpaceUsageApi = postSpaceUsageStub.secondCall.args[1];
+        expect(secondSpaceUsagePostedToMockSpaceUsageApi.variables.input)
+          .deep.equals(spaceId2ExpectedSpaceUsageToBeCalculated);
+      });
+    });
+
+    context('but space usage api is NOT available to save space usage,', function () {
+      it.only('should throw the error', async function () {
+        const postSpaceUsageError = new Error('an error');
+        postSpaceUsageStub.onFirstCall().throws(postSpaceUsageError);
+
+        process.once('unhandledRejection', (error) => {
+          expect(error.message).equals(postSpaceUsageError.message);
+        });
+
+        wifiRecordingsSpaceUsageCalculator.calculateSpaceUsage(calculateSpaceUsageParams);
+
+        await setPromisifiedTimeout(1);
+      });
     });
   });
 });
